9b3065c39ed4279f5d3f8fd4705388b11b3a7945
rahul <krahul8197@gmail.com>
2025-07-21 19:36:46 +0530

    refresh icon added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 
 import { useState } from 'react';
 import React from 'react';
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
+
+
 function App() {
   const [title, setTitle] = useState('');
 
@@ -19,14 +22,30 @@ function App() {
   }
 };
 
+
+  const refreshTitle = () => {
+    setTitle(''); // Clears the tab title
+  };
+
+
   return (
     <div className="p-4 font-sans w-100 ">
-      <button
-        onClick={getTabTitle}
-        className="bg-blue-700 text-white px-4 py-2 rounded hover:bg-blue-500"
-      >
-        Get Tab Title
-      </button>
+      <div className="flex gap-2">
+        <button
+          onClick={getTabTitle}
+          className="bg-blue-700 text-white px-4 py-2 rounded hover:bg-blue-500"
+        >
+          Get Tab Title
+        </button>
+
+        <button
+          onClick={refreshTitle}
+          className="bg-gray-200 text-white px-4 py-2 rounded hover:bg-gray-500"
+          title="Clear Title"
+        >
+          <ArrowPathIcon className="h-5 w-5 text-white" />
+        </button>
+      </div>
 
       <div className="w-80  items-center justify-center mt-4 mb-4 p-2 text-center break-words font-bold text-gray-800 text-sm  rounded-md border-1 border-blue-400">
         {title || 'Title will appear here'}
